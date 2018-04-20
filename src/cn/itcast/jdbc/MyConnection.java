248cf85dcb433720acc2c4f6fd5178f497254e73
jianliu2003 <jianliu2003@126.com>
2018-04-20 16:32:49 +0800

    first init

---
+++ src/cn/itcast/jdbc/MyConnection.java
@@ -0,0 +1,15 @@
+package cn.itcast.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MyConnection extends ConnectionWrapper {
+	public MyConnection(Connection con) {
+		super(con);
+	}
+	
+	public void close() throws SQLException {
+		System.out.println("哈哈~");
+		super.close();
+	}
+}
